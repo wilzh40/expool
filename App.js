6529725feb3fc2c6e88e71a6ff4398b028c88bda
wilzh40 <wilzh40@gmail.com>
2017-06-23 11:35:29 -0700

    Initial commit'

---
+++ App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {Engine, Render, World, Bodies} from 'matter-js'
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Open up main.js to start working on your app!</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
