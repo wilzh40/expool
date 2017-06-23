b40ba0a2e6db3aa9b67135fc996e186acca0f398
Jeffrey Da <jexelD@gmail.com>
2017-06-23 12:39:21 -0700

    Server code

--- App.js
+++ App.js
@@ -1,8 +1,26 @@
 import React from 'react';
-import {Engine, Render, World, Bodies} from 'matter-js'
+import { Engine, Render, World, Bodies } from 'matter-js'
 import { StyleSheet, Text, View } from 'react-native';
 
+const io = require('socket.io-client');
+const url = 'https://7de78ffe.ngrok.io';
+
 export default class App extends React.Component {
+
+  componentDidMount() {
+    // INSTRUCTION:
+    // or replace with your local ngrok url, eg: https://brent123.ngrok.io
+    // start ngrok with ngrok http 3000 --subdomain=brent123
+    // where the subdomain is whatever subdomain you want
+    const socket = io(url, {
+      transports: ['websocket'],
+    });
+
+    socket.on('connect', () => {
+      // do something
+    });
+  }
+
   render() {
     return (
       <View style={styles.container}>
